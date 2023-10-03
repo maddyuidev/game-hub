6cd99d9141172bbb9b84b1598ff8d79ec421e74e
Madhu babu <mbmadhurai@gmail.com>
2023-10-03 13:17:13 +0530

    Get Optimized images

---
+++ src/services/image-url.ts
@@ -0,0 +1,11 @@
+const getCroppedImageUrl = (
+  url: string,
+  width: number,
+  height: number
+): string => {
+  const target = "media/";
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
+};
+
+export default getCroppedImageUrl;
